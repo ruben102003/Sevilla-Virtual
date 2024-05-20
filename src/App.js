9be4cc11c50bd033ee02490593364f0618ee6fbe
AlumnoRuben <alumno.501481@ies-azarquiel.es>
2024-05-20 09:37:56 +0200

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import QuienesSomos from './components/QuienesSomos';
+import Novedades from './components/Novedades';
+import Alojamiento from './components/Alojamiento';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/quienes-somos" component={QuienesSomos} />
+          <Route path="/novedades" component={Novedades} />
+          <Route path="/alojamiento" component={Alojamiento} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
