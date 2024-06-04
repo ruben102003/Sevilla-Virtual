4bd0ac508fd2b9d5bc351671685d88183ea77605
AlumnoRuben <alumno.501481@ies-azarquiel.es>
2024-06-04 11:17:42 +0200

    17commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+// Importa la biblioteca React, necesaria para crear componentes
 import React from 'react';
+
+// Importa ReactDOM para renderizar componentes React en el DOM
 import ReactDOM from 'react-dom';
+
+// Importa el archivo de estilos CSS global para la aplicación
 import './index.css';
+
+// Importa el componente principal de la aplicación
 import App from './App';
+
+// Importa una función para medir el rendimiento de la aplicación
 import reportWebVitals from './reportWebVitals';
 
+// Renderiza el componente App dentro de un modo estricto de React para resaltar posibles problemas
 ReactDOM.render(
   <React.StrictMode>
+    {/* Renderiza el componente App */}
     <App />
   </React.StrictMode>,
+  // Selecciona el elemento del DOM con id 'root' como el contenedor para la aplicación
   document.getElementById('root')
 );
 
-
+// Llama a reportWebVitals para medir el rendimiento de la aplicación
 reportWebVitals();
